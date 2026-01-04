9267e7efc5cbc74ca94e3c265fa3875a3fceabfa
e5pe0n <1izard.synchr0fi5h@gmail.com>
2026-01-04 12:59:38 +0900

    feat: futex locks

---
+++ examples/futex_locks/main.rs
@@ -0,0 +1,204 @@
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use std::time::Duration;
+use std::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+    sync::atomic::AtomicU32,
+};
+use std::{thread, u32};
+
+use atomic_wait::{wait, wake_all, wake_one};
+
+struct Mutex<T> {
+    // 0: unlocked
+    // 1: locked
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T: Send> Sync for Mutex<T> {}
+
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> MutexGuard<T> {
+        while self.state.swap(1, Acquire) == 1 {
+            wait(&self.state, 1);
+        }
+        MutexGuard { mutex: self }
+    }
+}
+
+struct MutexGuard<'a, T> {
+    mutex: &'a Mutex<T>,
+}
+
+impl<T> Deref for MutexGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.mutex.value.get() }
+    }
+}
+
+impl<T> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.mutex.value.get() }
+    }
+}
+
+impl<T> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.mutex.state.store(0, Release);
+        wake_one(&self.mutex.state);
+    }
+}
+
+struct Condvar {
+    counter: AtomicU32,
+}
+
+impl Condvar {
+    pub const fn new() -> Self {
+        Self {
+            counter: AtomicU32::new(0),
+        }
+    }
+
+    pub fn notify_one(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_one(&self.counter);
+    }
+
+    pub fn notify_all(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_all(&self.counter);
+    }
+
+    pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        let counter_value = self.counter.load(Relaxed);
+
+        let mutex = guard.mutex;
+        drop(guard);
+
+        wait(&self.counter, counter_value);
+
+        mutex.lock()
+    }
+}
+
+struct RwLock<T> {
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T: Send + Sync> Sync for RwLock<T> {}
+
+impl<T> RwLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn read(&self) -> ReadGuard<T> {
+        let mut s = self.state.load(Relaxed);
+        loop {
+            if s < u32::MAX {
+                assert!(s != u32::MAX - 1, "too many readers");
+                match self.state.compare_exchange_weak(s, s + 1, Acquire, Relaxed) {
+                    Ok(_) => return ReadGuard { rwlock: self },
+                    Err(e) => s = e,
+                }
+            }
+            if s == u32::MAX {
+                wait(&self.state, u32::MAX);
+                s = self.state.load(Relaxed);
+            }
+        }
+    }
+
+    pub fn write(&self) -> WriteGuard<T> {
+        while let Err(s) = self.state.compare_exchange(0, u32::MAX, Acquire, Relaxed) {
+            wait(&self.state, s);
+        }
+        WriteGuard { rwlock: self }
+    }
+}
+
+struct ReadGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for ReadGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        if self.rwlock.state.fetch_sub(1, Release) == 1 {
+            wake_one(&self.rwlock.state);
+        }
+    }
+}
+
+struct WriteGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for WriteGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> DerefMut for WriteGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.rwlock.value.get() }
+    }
+}
+
+impl<T> Drop for WriteGuard<'_, T> {
+    fn drop(&mut self) {
+        self.rwlock.state.store(0, Release);
+        wake_all(&self.rwlock.state);
+    }
+}
+
+fn main() {}
+
+#[test]
+fn test_condvar() {
+    let mutex = Mutex::new(0);
+    let condvar = Condvar::new();
+
+    let mut wakeups = 0;
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            thread::sleep(Duration::from_secs(1));
+            *mutex.lock() = 123;
+            condvar.notify_one();
+        });
+
+        let mut m = mutex.lock();
+        while *m < 100 {
+            m = condvar.wait(m);
+            wakeups += 1;
+        }
+
+        assert_eq!(*m, 123);
+    });
+}
